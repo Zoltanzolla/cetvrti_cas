d21da678b93954a22d417072c2b1933d53e7d324
Zoltan <zoltan.nacuk@gmail.com>
2018-06-23 20:44:55 +0200

    vezbe 4

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
+$prodavnica = array(
+  array('ID' => '1','ime_proizvoda' => 'mleko','cena' => '80','kupac' => '1','marka' => 'mlekarasu'),
+  array('ID' => '2','ime_proizvoda' => 'hleb','cena' => '60','kupac' => '2','marka' => 'fidelinka'),
+  array('ID' => '3','ime_proizvoda' => 'meso','cena' => '300','kupac' => '3','marka' => 'zlatiborac'),
+  array('ID' => '4','ime_proizvoda' => 'voda','cena' => '120','kupac' => '4','marka' => 'vodavoda'),
+  array('ID' => '5','ime_proizvoda' => 'salata','cena' => '70','kupac' => '5','marka' => 'gazdinstvo'),
+  array('ID' => '6','ime_proizvoda' => 'sapun','cena' => '50','kupac' => '6','marka' => 'henkel'),
+  array('ID' => '7','ime_proizvoda' => 'cokolada','cena' => '100','kupac' => '7','marka' => 'nestle'),
+  array('ID' => '8','ime_proizvoda' => 'sladoled','cena' => '150','kupac' => '8','marka' => 'frikom'));
+
+
 
 ?>
